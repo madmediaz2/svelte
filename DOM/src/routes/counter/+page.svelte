0a68e0540c6737bc27abfd56e61910163dba3cde
madmediaz2 <simar.cakmak@gmail.com>
2024-02-13 01:02:33 +0100

    Added Counter

---
+++ DOM/src/routes/counter/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+
+  let count = 0;
+  let minValue = 0;
+  let maxValue = 10;
+  let interval;
+
+  function modifyCount(amount) {
+    
+    if (count < minValue || count > maxValue) {
+        assert(`Count: ${count} should be between ${minValue} and ${maxValue}.`);
+        if (count < minValue) {
+          count = minValue;
+        }
+        else {
+          count = maxValue;
+        }
+    }
+    else {
+      count += amount;
+    }
+  }
+
+  function assert(message) {
+    console.error(message);
+    clearInterval(interval);
+    // Reset assert text after 5 seconds
+    setTimeout(() => {
+    assertText = "";
+    }, 5000);
+    assertText = message;
+
+  }
+
+  function startInterval(amount) {
+    clearInterval(interval);
+    interval = setInterval(() => modifyCount(amount), 200);
+  }
+
+  // Cleanup interval when component is destroyed
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+
+  let assertText = "";
+</script>
+
+<section class="text-center">
+  <div class="card w-100">
+    <div class="card-body">
+      <h5 class="card-title">Simple Counter</h5>
+      <p class="card-text">{assertText}</p>
+      <button id="increment" class="btn btn-primary" 
+        on:mousedown={() => modifyCount(1)}
+        on:mousedown={() => startInterval(1)}
+        on:mouseup={() => clearInterval(interval)}
+        on:mouseleave={() => clearInterval(interval)}>
+        Increment
+      </button>
+      <button id="decrement" class="btn btn-primary" 
+        on:mousedown={() => modifyCount(-1)}
+        on:mousedown={() => startInterval(-1)}
+        on:mouseup={() => clearInterval(interval)}
+        on:mouseleave={() => clearInterval(interval)}>
+        Decrement
+      </button>
+      <p class="card-text">Counter: <strong>{count}</strong></p>
+    </div>
+  </div>
+</section>
+
+<style>
+  /* Your styles here */
+</style>
